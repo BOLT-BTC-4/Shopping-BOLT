09e32a5f5d3c3189a3edcd88766228ea25e73e4e
SatoruKikuchi123 <satoru_kikuchi@mail.toyota.co.jp>
2023-06-22 18:41:31 +0900

    mastrItemTable追加

--- src/screen/Main.jsx
+++ src/screen/Main.jsx
@@ -21,6 +21,7 @@ export const Main = () => {
     getItems();
   }, []);
   const [trashFlag, setTrashFlag] = useState(false);
+  const [selected, setSelected] = useState("");
 
   const getItems = () => {
     setItems([
@@ -40,41 +41,8 @@ export const Main = () => {
         directions: 3,
         check: false,
       },
-      {
-        sales: "野菜",
-        itemName: "玉ねぎ",
-        quantity: 2,
-        unit: "個",
-        directions: 1,
-        check: false,
-      },
-      {
-        sales: "肉",
-        itemName: "鶏肉",
-        quantity: 300,
-        unit: "g",
-        directions: 3,
-        check: false,
-      },
-      {
-        sales: "野菜",
-        itemName: "玉ねぎ",
-        quantity: 2,
-        unit: "個",
-        directions: 1,
-        check: false,
-      },
-      {
-        sales: "肉",
-        itemName: "鶏肉",
-        quantity: 300,
-        unit: "g",
-        directions: 3,
-        check: false,
-      },
     ]);
   };
-  const [selected, setSelected] = useState("");
 
   const data = [
     { key: "1", value: "Mobiles", disabled: true },
@@ -85,6 +53,130 @@ export const Main = () => {
     { key: "6", value: "Diary Products" },
     { key: "7", value: "Drinks" },
   ];
+
+  const masterItemTable = [
+    {
+      sales: "野菜",
+      itemName: "玉ねぎ",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "果物",
+      itemName: "リンゴ",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "お肉",
+      itemName: "鶏肉",
+      quantity: 200,
+      unit: "g",
+    },
+    {
+      sales: "卵",
+      itemName: "卵",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "お魚",
+      itemName: "サンマ",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "乳製品",
+      itemName: "ヨーグルト",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "冷凍",
+      itemName: "アイス",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "大豆類",
+      itemName: "納豆",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "お菓子",
+      itemName: "ポテチ",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "パン",
+      itemName: "食パン",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "ジャム",
+      itemName: "いちごジャム",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "お米",
+      itemName: "米",
+      quantity: 10,
+      unit: "kg",
+    },
+    {
+      sales: "麺類",
+      itemName: "パスタ",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "惣菜",
+      itemName: "コロッケ",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "調味料",
+      itemName: "胡椒",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "飲料・酒",
+      itemName: "ビール",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "日用品",
+      itemName: "ごみ袋",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "生活雑貨",
+      itemName: "フライ返し",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "健康",
+      itemName: "サプリメント",
+      quantity: 1,
+      unit: "個",
+    },
+    {
+      sales: "介護・ベビー",
+      itemName: "おむつ",
+      quantity: 1,
+      unit: "個",
+    },
+  ];
+
   return (
     <View style={styles.container}>
       <SelectList
