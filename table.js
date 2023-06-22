e586bc9a5aea61e2504a9f1729a6d7b625a1ce73
SatoruKikuchi123 <satoru_kikuchi@mail.toyota.co.jp>
2023-06-23 08:49:25 +0900

    mainにあったmasterItemTableをこのファイルに移動

---
+++ table.js
@@ -0,0 +1,126 @@
+const masterItemTable = [
+  {
+    sales: "野菜",
+    itemName: "玉ねぎ",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "果物",
+    itemName: "リンゴ",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "お肉",
+    itemName: "鶏肉",
+    quantity: 200,
+    unit: "g",
+  },
+  {
+    sales: "卵",
+    itemName: "卵",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "お魚",
+    itemName: "サンマ",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "乳製品",
+    itemName: "ヨーグルト",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "冷凍",
+    itemName: "アイス",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "大豆類",
+    itemName: "納豆",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "お菓子",
+    itemName: "ポテチ",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "パン",
+    itemName: "食パン",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "ジャム",
+    itemName: "いちごジャム",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "お米",
+    itemName: "米",
+    quantity: 10,
+    unit: "kg",
+  },
+  {
+    sales: "麺類",
+    itemName: "パスタ",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "惣菜",
+    itemName: "コロッケ",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "調味料",
+    itemName: "胡椒",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "飲料・酒",
+    itemName: "ビール",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "日用品",
+    itemName: "ごみ袋",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "生活雑貨",
+    itemName: "フライ返し",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "健康",
+    itemName: "サプリメント",
+    quantity: 1,
+    unit: "個",
+  },
+  {
+    sales: "介護・ベビー",
+    itemName: "おむつ",
+    quantity: 1,
+    unit: "個",
+  },
+];
+
+export const table = {
+  masterItem: masterItemTable,
+};
