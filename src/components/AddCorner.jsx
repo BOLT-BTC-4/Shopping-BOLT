d265d30ff08fd4ba2c13f28632e5b72bd50a5403
tmc-yukihide-ohta <yukihide_ohta@mail.toyota.co.jp>
2023-06-23 06:44:43 +0900

    add:売場登録のリスト表示と追加機能実装

--- src/components/AddCorner.jsx
+++ src/components/AddCorner.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 
-export const AddCorner = () => {
+export const AddCorner = (props) => {
+  console.log("=====AddCorner側の処理です=====");
+  const { filteredCorners, setCorners, selectedValue, setSelectedValue } =
+    props;
+  console.log(filteredCorners);
+  console.log(setCorners);
   return (
     <View>
       <Text>AddCornerだよ！</Text>
+      <Picker
+        style={{ width: 200 }}
+        selectedValue={selectedValue}
+        onValueChange={(itemValue) => setSelectedValue(itemValue)}
+      >
+        {filteredCorners.map((option, index) => (
+          <Picker.Item key={index} label={option} value={option} />
+        ))}
+      </Picker>
     </View>
   );
 };
--- src/screen/EditShop.jsx
+++ src/screen/EditShop.jsx
@@ -2,69 +2,126 @@ import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
+  TextInput,
   View,
   TouchableOpacity,
   FlatList,
   Button,
+  Modal,
 } from "react-native";
 import { CornerList } from "../components/CornerList";
 import { AddCorner } from "../components/AddCorner";
 
-export const EditShop = () => {
+export const EditShop = (props) => {
+  const { navigation } = props;
+
   const testShop = {
     shopName: "イオン 安城店",
     corners: ["野菜", "お肉", "お魚"],
   };
 
-  const getAllCorners = () => {
-    const AllCorners = ["野菜", "果物", "お魚", "お肉"];
+  // useState一覧
+  const [modalVisible, setModalVisible] = useState(false);
+  const [corners, setCorners] = useState(testShop.corners);
+  const [shopName, setShopName] = useState(testShop.shopName);
+  const [selectedValue, setSelectedValue] = useState("");
+  const [shop, setShop] = useState(testShop);
+
+  const openModal = () => {
+    setModalVisible(true);
   };
 
-  const [corners, setCorners] = useState(testShop.corners);
-  const [shop, setShop] = useState(testShop);
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  const getAllCorners = () => {
+    return ["野菜", "果物", "お魚", "お肉", "日用品", "お菓子", "お惣菜"];
+  };
+  console.log("getAllCorners:", getAllCorners());
+
+  const filteredCorners = () => {
+    console.log("=====追加ボタン押されました======");
+    const allCorners = getAllCorners();
+    // getAllCorners配列にcornerが含まれていない場合に真となり配列に追加される
+    const result = allCorners.filter((corner) => !corners.includes(corner));
+    console.log("result", result);
+    return result;
+  };
+
+  const handleCornersChange = () => {
+    const newCorners = [...corners]; // 現在のcornersのコピーを作成
+    newCorners.push(selectedValue); // 新しい値を追加
+    setCorners(newCorners); // 変更された値をセット
+  };
+
+  const handleInputChange = (value) => {
+    setShopName(value);
+  };
+  const handleButtonPress = () => {
+    // 入力値の処理
+    console.log(shopName);
+    console.log(corners);
+  };
 
   useEffect(() => {
     // getItems();
   }, []);
 
-  console.log(corners);
-  console.log(shop);
-
   return (
     <View style={styles.EditShopContainer}>
-      <View>
-        <Text>{shop.shopName}</Text>
+      <View style={styles.container}>
+        <View>
+          <TextInput
+            style={styles.input}
+            value={shopName}
+            onChangeText={handleInputChange}
+            placeholder="入力してください"
+          />
+        </View>
+        <View>
+          <FlatList
+            data={corners}
+            renderItem={({ item }) => <CornerList cornerName={item} />}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        </View>
       </View>
-      <View>
-        <FlatList
-          data={corners}
-          renderItem={({ item }) => <CornerList cornerName={item} />}
-          keyExtractor={(item, index) => index.toString()}
-        />
-      </View>
-      <View>
-        <Text>登録</Text>
-      </View>
-      <View>
-        <AddCorner />
-      </View>
-      <View>
-        <TouchableOpacity
-          onLongPress={() => {
-            alert("長押しタップ成功！");
-          }}
-        >
-          <Text
-            style={{
-              color: "white",
-              backgroundColor: "blue",
-              height: 30,
-              fontSize: 30,
+      <View style={styles.bottomContainer}>
+        <View>
+          <Button title="登録ボタン" onPress={handleButtonPress} />
+          <Button
+            title="追加"
+            onPress={() => {
+              openModal();
+              filteredCorners();
             }}
-          >
-            ここを長押ししてください
-          </Text>
-        </TouchableOpacity>
+          />
+        </View>
+        <View>
+          <Modal visible={modalVisible} animationType="none" transparent={true}>
+            <View style={styles.modalContainer}>
+              <View style={styles.modalContent}>
+                <AddCorner
+                  filteredCorners={filteredCorners()}
+                  setCorners={setCorners}
+                  selectedValue={selectedValue}
+                  setSelectedValue={setSelectedValue}
+                />
+                <View style={styles.buttonContainer}>
+                  <Button title="キャンセル" onPress={closeModal} />
+                  <Button
+                    title="OK"
+                    onPress={() => {
+                      closeModal();
+                      handleCornersChange();
+                    }}
+                  />
+                </View>
+              </View>
+            </View>
+          </Modal>
+        </View>
       </View>
     </View>
   );
@@ -72,6 +129,62 @@ export const EditShop = () => {
 
 const styles = StyleSheet.create({
   EditShopContainer: {
-    backgroundColor: "#f0f4f8",
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  container: {
+    // flex: 1,
+    height: 425,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "#ffffff",
+    width: 300,
+    padding: 20,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row", // 横方向に配置
+    justifyContent: "space-between", // ボタンを均等に配置
+    marginTop: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+  bottomContainer: {
+    marginBottom: 40,
+    paddingHorizontal: 16,
   },
 });
+
+{
+  /* <View>
+<TouchableOpacity
+  onLongPress={() => {
+    alert("長押しタップ成功！");
+  }}
+>
+  <Text
+    style={{
+      color: "white",
+      backgroundColor: "blue",
+      height: 30,
+      fontSize: 30,
+    }}
+  >
+    ここを長押ししてください
+  </Text>
+</TouchableOpacity>
+</View> */
+}
