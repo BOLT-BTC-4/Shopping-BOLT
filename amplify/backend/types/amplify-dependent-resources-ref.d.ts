94fd615eb53ea145e22391de60f046f764c2d9ef
SatoruKikuchi123 <satoru_kikuchi@mail.toyota.co.jp>
2023-06-22 11:16:07 +0900

    tutorial作成

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "shoppingbolt2": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
