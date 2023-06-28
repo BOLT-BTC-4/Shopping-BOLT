bcd73bb70c86a4b0979acd81d22aa0d521725b3d
tmc-yukihide-ohta <yukihide_ohta@mail.toyota.co.jp>
2023-06-28 23:44:51 +0900

    タブ追加による、データ移行（MainとShop途中状態）

---
+++ src/components/ShopComponents/ShopList.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+import { Feather } from "@expo/vector-icons";
+import { StyleSheet, Text, View, Modal, Button } from "react-native";
+// import { ShareShopDataContext } from "./ShareShopDataContext";
+
+export const ShopList = ({
+  value,
+  handleCheck,
+  handleRemoveItem,
+  items,
+  setItems,
+  setAddFlag,
+}) => {
+  //モーダルのuseState
+  const [modalEditItemVisible, setModalEditItemVisible] = useState(false);
+  return (
+    <View>
+      <View style={styles.moziBox}>
+        <Text style={styles.text}>{value}</Text>
+      </View>
+      <View style={styles.iconBox}>
+        <Feather
+          name="edit"
+          size={24}
+          color="black"
+          onPress={() => setModalEditItemVisible(true)}
+        />
+        <Feather
+          name="trash-2"
+          size={24}
+          color="black"
+          onPress={() => handleRemoveItem(item.localId)}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  box: {
+    height: 50,
+    width: "100%",
+    borderWidth: 1,
+    borderBottomColor: "mediumseagreen",
+    borderLeftColor: "rgba(0,0,0,0)",
+    borderRightColor: "rgba(0,0,0,0)",
+    borderTopColor: "rgba(0,0,0,0)",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+
+  check: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    textDecorationLine: "line-through",
+  },
+
+  moziBox: {
+    flex: 1,
+    // backgroundColor: "steelblue",
+    padding: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+
+  salesBox: {
+    width: 80,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  iconBox: {
+    width: 100,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 16,
+  },
+
+  subText: {
+    fontSize: 12,
+    color: "red",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContents: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
