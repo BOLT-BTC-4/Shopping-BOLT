38bff52f3630731660784f3f4799f5bafe70bbe2
SatoruKikuchi123 <satoru_kikuchi@mail.toyota.co.jp>
2023-06-30 11:06:22 +0900

    ユーザー認証追加

--- App.js
+++ App.js
@@ -11,8 +11,9 @@ import {
 // import { API, graphqlOperation } from "aws-amplify";
 // import { createTodo } from "./src/graphql/mutations";
 // import { listTodos } from "./src/graphql/queries";
-// import { Amplify } from "aws-amplify";
-// import awsExports from "./src/aws-exports";
+import { Amplify } from "aws-amplify";
+import awsExports from "./src/aws-exports";
+Amplify.configure(awsExports);
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -30,8 +31,24 @@ import { ShopList } from "./src/components/ShopComponents/ShopList";
 import { EditMenu } from "./src/components/MenuComponents/EditMenu";
 import { EditShop } from "./src/components/ShopComponents/EditShop";
 import { AddMenu } from "./src/components/MenuComponents/AddMenu";
+import {
+  withAuthenticator,
+  useAuthenticator,
+} from "@aws-amplify/ui-react-native";
 
-// Amplify.configure(awsExports);
+// retrieves only the current value of 'user' from 'useAuthenticator'
+const userSelector = (context) => [context.user];
+
+const SignOutButton = () => {
+  const { user, signOut } = useAuthenticator(userSelector);
+  return (
+    <Pressable onPress={signOut} style={styles.buttonContainer}>
+      <Text style={styles.buttonText}>
+        Hello, {user.username}! Click here to sign out!
+      </Text>
+    </Pressable>
+  );
+};
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -74,59 +91,81 @@ const ShopStack = () => {
   );
 };
 
-export default App = () => {
+const App = () => {
   return (
-    <ShareShopDataProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          initialRouteName="買物"
-          activeColor="#f0edf6"
-          inactiveColor="#3e2465"
-          barStyle={{ backgroundColor: "#694fad" }}
-        >
-          <Tab.Screen
-            name="献立"
-            component={MenuStack}
-            options={{
-              tabBarLabel: "献立",
-              tabBarIcon: ({ color }) => (
-                <AntDesign name="calendar" color={color} size={26} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="レシピ"
-            component={RecipeStack}
-            options={{
-              tabBarLabel: "レシピ",
-              tabBarIcon: ({ color }) => (
-                <FontAwesome5 name="utensils" color={color} size={26} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="買物"
-            component={MainStack}
-            options={{
-              tabBarLabel: "買物",
-              tabBarIcon: ({ color }) => (
-                <AntDesign name="shoppingcart" color={color} size={26} />
-              ),
-            }}
-          />
+    <>
+      <SignOutButton />
+      <ShareShopDataProvider>
+        <NavigationContainer>
+          <Tab.Navigator
+            initialRouteName="買物"
+            activeColor="#f0edf6"
+            inactiveColor="#3e2465"
+            barStyle={{ backgroundColor: "#694fad" }}
+          >
+            <Tab.Screen
+              name="献立"
+              component={MenuStack}
+              options={{
+                tabBarLabel: "献立",
+                tabBarIcon: ({ color }) => (
+                  <AntDesign name="calendar" color={color} size={26} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="レシピ"
+              component={RecipeStack}
+              options={{
+                tabBarLabel: "レシピ",
+                tabBarIcon: ({ color }) => (
+                  <FontAwesome5 name="utensils" color={color} size={26} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="買物"
+              component={MainStack}
+              options={{
+                tabBarLabel: "買物",
+                tabBarIcon: ({ color }) => (
+                  <AntDesign name="shoppingcart" color={color} size={26} />
+                ),
+              }}
+            />
 
-          <Tab.Screen
-            name="お店"
-            component={ShopStack}
-            options={{
-              tabBarLabel: "お店",
-              tabBarIcon: ({ color }) => (
-                <Entypo name="shop" color={color} size={26} />
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </ShareShopDataProvider>
+            <Tab.Screen
+              name="お店"
+              component={ShopStack}
+              options={{
+                tabBarLabel: "お店",
+                tabBarIcon: ({ color }) => (
+                  <Entypo name="shop" color={color} size={26} />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ShareShopDataProvider>
+    </>
   );
 };
+export default withAuthenticator(App);
+
+const styles = StyleSheet.create({
+  container: { width: 400, flex: 1, padding: 20, alignSelf: "center" },
+  todo: { marginBottom: 15 },
+  input: {
+    backgroundColor: "#ddd",
+    marginBottom: 10,
+    padding: 8,
+    fontSize: 18,
+  },
+  todoName: { fontSize: 20, fontWeight: "bold" },
+  buttonContainer: {
+    alignSelf: "center",
+    backgroundColor: "black",
+    paddingHorizontal: 8,
+  },
+  buttonText: { color: "white", padding: 16, fontSize: 18 },
+});
