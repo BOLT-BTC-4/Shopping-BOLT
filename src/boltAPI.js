277855cb94c42a2c385eeec04022206bcd6184d1
ayano_kondo@mail.toyota.co.jp <aya-no>
2023-07-01 22:40:31 +0900

    レシピの取得・削除API追加

--- src/boltAPI.js
+++ src/boltAPI.js
@@ -24,7 +24,7 @@ export const createShopAPI = async (data) => {
   }
 };
 
-// Shop一覧の取得
+// Shop　お店一覧の取得
 export const fetchShopAPI = async () => {
   try {
     const shopList = await DataStore.query(Shop);
@@ -154,3 +154,59 @@ export const createRecipeAPI = async (data) => {
     throw error;
   }
 };
+
+// recipe レシピ一覧の取得
+export const fetchRecipeAPI = async () => {
+  try {
+    const recipeList = await DataStore.query(Recipe);
+    return JSON.stringify(recipeList, null, 2);
+  } catch (err) {
+    throw err;
+  }
+}
+
+// recipe IDを指定してレシピの取得
+export const fetchIdRecipeAPI = async (id) => {
+  try {
+    const recipe = await DataStore.query(Recipe, id);
+    // const recipeItem = recipeList[0].RecipeItems.values.then(item => item = JSON.stringify(item, null, 2))
+    return JSON.stringify(recipe, null, 2);
+  } catch (err) {
+    throw err;
+  }
+}
+
+// recipeItem recipeIdを指定してレシピ材料の取得
+export const fetchIdRecipeItemAPI = async (id) => {
+  try {
+    const recipeItem = await DataStore.query(RecipeItem, r => r.recipeID.eq(id));
+    console.log(recipeItem)
+    return JSON.stringify(recipeItem, null, 2);
+  } catch (err) {
+    throw err;
+  }
+}
+
+// レシピの削除
+export const deleteRecipeAPI = async (id) => {
+  try {
+    const deleterecipe = await DataStore.query(Recipe, id);
+    DataStore.delete(Recipe, deleterecipe)
+
+  } catch (err) {
+    throw err;
+  }
+}
+
+
+
+// ログアウト時にローカルデータをクリアする 
+// ＊同じ端末で別ユーザーログイン時、ローカルデータを消さないと前のログインユーザーのデータが見えてしまう。
+export const dataClearAPI = async () => {
+  try {
+    await DataStore.clear();
+
+  } catch (err) {
+    throw err;
+  }
+}
