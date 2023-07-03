bb389a6b41c4250d7ecd951480f7b496ec504541
tmc-yukihide-ohta <yukihide_ohta@mail.toyota.co.jp>
2023-07-03 20:26:09 +0900

    マージ

--- src/screen/RecipeScreen.jsx
+++ src/screen/RecipeScreen.jsx
@@ -13,6 +13,7 @@ import { RecipeList } from "../components/RecipeComponents/RecipeList.jsx";
 import { ShareShopDataContext } from "./ShareShopDataContext";
 import { MaterialIcons } from "@expo/vector-icons";
 import { fetchRecipeAPI, deleteRecipeAPI } from "../boltAPI";
+import { FlatGrid } from "react-native-super-grid";
 
 export const RecipeScreen = ({ navigation }) => {
   console.log("===== comp_RecipeScreen =====");
@@ -24,6 +25,34 @@ export const RecipeScreen = ({ navigation }) => {
     const initRecipeData = await fetchRecipeAPI();
     setRecipeData(initRecipeData);
   };
+
+  const [selectedCategory, setSelectedCategory] = useState(1);
+  const [displayedRecipes, setDisplayedRecipes] = useState([]);
+
+  // カテゴリ
+  const categories = [
+    { id: 1, categry: "主食" },
+    { id: 2, categry: "主菜" },
+    { id: 3, categry: "副菜" },
+    { id: 4, categry: "汁物" },
+    { id: 5, categry: "その他" },
+  ];
+
+  //カテゴリが選択されたらそのカテゴリに該当するレシピを表示
+  const handleCategorySelect = (categoryId) => {
+    setSelectedCategory(categoryId);
+    setDisplayedRecipes(defaultRecipes[categoryId]);
+  };
+
+  //カテゴリタブ表示
+  const renderCategoryTab = ({ item }) => (
+    <TouchableOpacity
+      style={selectedCategory === item.id ? styles.activeTab : styles.tab}
+      onPress={() => handleCategorySelect(item.id)}
+    >
+      <Text>{item.categry}</Text>
+    </TouchableOpacity>
+  );
 
   useEffect(() => {
     // レシピの一覧を取得
