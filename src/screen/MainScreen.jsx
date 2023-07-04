37034aa10f9e7560a781ad55204e151182a7e4af
ayano_kondo@mail.toyota.co.jp <aya-no>
2023-07-04 14:25:51 +0900

    買い物リスト削除修正（バックと繋げる）

--- src/screen/MainScreen.jsx
+++ src/screen/MainScreen.jsx
@@ -18,6 +18,7 @@ import {
   Modal,
   TouchableOpacity,
 } from "react-native";
+import { fetchShoppingListAPI, deleteShoppingListAPI } from "../boltAPI";
 
 export const MainScreen = () => {
   console.log("===== comp_MainScreen =====");
@@ -35,14 +36,14 @@ export const MainScreen = () => {
   const { shopData, setShopData } = useContext(ShareShopDataContext);
   const { shopDataDrop, setShopDataDrop } = useContext(ShareShopDataContext);
 
-  //初回のみデフォルトのitemsデータを取得
   useEffect(() => {
-    getItems();
-    // handleButtonClick();
+    // 買い物リスト一覧の取得
+    const getAllShoppingList = async () => {
+      const shoppingLIstData = await fetchShoppingListAPI();
+      setItems(shoppingLIstData);
+    };
+    getAllShoppingList();
   }, []);
-  const getItems = () => {
-    setItems(table.defaultItems);
-  };
 
   // モーダルのuseState
   const [modalAddItemVisible, setModalAddItemVisible] = useState(false);
@@ -54,9 +55,11 @@ export const MainScreen = () => {
     setItems(newItems);
   };
 
-  const handleRemoveItem = (localId) => {
-    const newItems = items.filter((item) => item.localId !== localId);
-    setItems(newItems);
+  // 選択した買い物リストアイテムの削除 → 買い物リスト一覧の取得
+  const handleRemoveItem = async (id) => {
+    await deleteShoppingListAPI(id);
+    const shoppingListData = await fetchShoppingListAPI();
+    setItems(shoppingListData);
   };
 
   const handleAllRemoveItem = () => {
