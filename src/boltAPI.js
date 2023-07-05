a1982308c8376550e30c7dc7b6ae92e445cd8be3
ayano_kondo@mail.toyota.co.jp <aya-no>
2023-07-05 14:48:11 +0900

    レシピとレシピ材料取得のAPI追加

--- src/boltAPI.js
+++ src/boltAPI.js
@@ -197,6 +197,26 @@ export const createMenuAPI = async (data) => {
   }
 };
 
+// Recipe IDからレシピとレシピ材料の取得
+export const fetchRecipeItemAPI = async (id) => {
+  try {
+    const recipe = await DataStore.query(Recipe, id);
+    const recipeItem = await DataStore.query(RecipeItem, (r) => r.recipeID.eq(recipe.id));
+    return {
+      id: recipe.id,
+      recipeName: recipe.recipeName,
+      memo: recipe.memo,
+      category: recipe.category,
+      url: recipe.url,
+      serving: recipe.serving,
+      like: recipe.like,
+      items: recipeItem
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
 // recipe レシピ一覧の取得
 export const fetchRecipeAPI = async () => {
   try {
