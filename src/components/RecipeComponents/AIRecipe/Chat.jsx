8e23503243980dbe06cda8bdd88acbee673f8547
tmc-yukihide-ohta <yukihide_ohta@mail.toyota.co.jp>
2023-07-07 11:39:05 +0900

    AIsレシピとりあえずレシピ登録までできるようにした。エラーや不具合は未解決状態

---
+++ src/components/RecipeComponents/AIRecipe/Chat.jsx
@@ -0,0 +1,44 @@
+import { Configuration, OpenAIApi } from "openai";
+import axios from "axios";
+
+// const configuration = new Configuration({
+//     organization: "org-sFBMY1Eruuh54fbnyyyn4Kq3",
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
+
+const API_URL = "https://api.openai.com/v1/";
+const MODEL = "gpt-3.5-turbo";
+const API_KEY = "APIキーをここに入れる";
+
+export const Chat = async (message) => {
+  try {
+    const response = await axios.post(
+      `${API_URL}chat/completions`,
+      {
+        // モデル ID の指定
+        model: MODEL,
+        // 質問内容
+        messages: [
+          {
+            role: "user",
+            content: message,
+          },
+        ],
+      },
+      {
+        // 送信する HTTP ヘッダー(認証情報)
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${API_KEY}`,
+        },
+      }
+    );
+    // 回答の取得
+    return response.data.choices[0].message.content;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
---
+++ src/components/RecipeComponents/AIRecipe/gptSelectValue.js
@@ -0,0 +1,56 @@
+const selectTargetData = [
+    "家族",
+    "大人",
+    "子ども",
+    "男性",
+    "女性",
+    "大学生",
+    "高校生",
+    "小学生",
+];
+
+const selectFeelingData = [
+    "喜びそうな",
+    "ワクワクしそうな",
+    "大好きな",
+    "大絶賛するような",
+    "リピートしたくなる",
+];
+
+const selectGenreData = [
+    "おすすめの",
+    "高級な",
+    "和風の",
+    "洋風の",
+    "イタリアンの",
+    "中華風の",
+];
+
+const selectCategoryData = [
+    "主食",
+    "主菜",
+    "副菜",
+    "汁物",
+    "スイーツ",
+];
+
+const selectServingData = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+];
+
+export const selectData = {
+    selectTargetData: selectTargetData,
+    selectFeelingData: selectFeelingData,
+    selectGenreData: selectGenreData,
+    selectCategoryData: selectCategoryData,
+    selectServingData: selectServingData,
+};
