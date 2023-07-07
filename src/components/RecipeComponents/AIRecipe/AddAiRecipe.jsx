33b0f82d125a52243ff341e15058582089bb2a17
tmc-yukihide-ohta <yukihide_ohta@mail.toyota.co.jp>
2023-07-07 23:31:59 +0900

    AIレシピのスタイルシート完成

--- src/components/RecipeComponents/AIRecipe/AddAiRecipe.jsx
+++ src/components/RecipeComponents/AIRecipe/AddAiRecipe.jsx
@@ -14,7 +14,7 @@ import { selectData } from "./gptSelectValue";
 import { useForm, Controller } from "react-hook-form";
 import { createRecipeAPI, fetchRecipeAPI } from "../../../boltAPI";
 import { ShareShopDataContext } from "../../../screen/ShareShopDataContext";
-import { RecipeList } from "../RecipeList";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 export const AddAiRecipe = ({ navigation }) => {
   const {
@@ -33,8 +33,8 @@ export const AddAiRecipe = ({ navigation }) => {
     formState: { errors },
   } = useForm();
 
+  // useState、useContext
   const { setRecipeData } = useContext(ShareShopDataContext);
-
   const [answer, setAnswer] = useState(false);
   const [jsAnswer, setJSAnswer] = useState("");
   const [userInputItem, setUserInputItem] = useState("");
@@ -44,7 +44,10 @@ export const AddAiRecipe = ({ navigation }) => {
   const [selectedCategory, setSelectedCategory] = useState("主菜");
   const [selectedServing, setSelectedServing] = useState(4);
   const [questionText, setQuestionText] = useState("");
+  // ローディング表示用のステート
+  const [loading, setLoading] = useState(false);
 
+  // プルダウンメニューやインプットボックスの値を変更した時の動作
   const handleMessageChange = (value) => {
     setUserInputItem(value);
   };
@@ -69,14 +72,11 @@ export const AddAiRecipe = ({ navigation }) => {
     setSelectedServing(value);
   };
 
-  const [tmpCheck, setTmpCheck] = useState("");
-
   const handleSubmit = async () => {
     setAnswer(false);
     console.log("Chat-GPTに考えてもらっています");
 
-    // 入力値の取得
-    // const selectedIngredient = message;
+    // 入力値を取得
     const userInputItemText = userInputItem;
     const selectedTargetText = selectedTarget;
     const selectedFeelingText = selectedFeeling;
@@ -85,7 +85,9 @@ export const AddAiRecipe = ({ navigation }) => {
     const selectedServingText = selectedServing;
 
     // 文章の組み立て
+    // ユーザが見る文章
     const displayText = `${userInputItemText}を使った、${selectedTargetText}が${selectedFeelingText}、${selectedGenreText}${selectedCategoryText}のレシピ、${selectedServingText}人前`;
+    // GPTに送る文章
     const generatedText = `${displayText}を教えてください。①レシピ名、②食材、③分量、④単位、⑤つくり方を教えて下さい。作り方は、工程ごとで区切って教えてください。必ず分量と単位は分けてください。分量には文字を入れず単位に文字を入れてください。①から⑤の情報をJavaScriptで利用できるように下記の配列とオブジェクトのフォーマットで、必ずJSON形式で回答をしてください。
     {
       recipeName:①レシピ名,
@@ -94,25 +96,29 @@ export const AddAiRecipe = ({ navigation }) => {
       memo:⑤作り方,
     }`;
     setQuestionText(displayText);
-    setTmpCheck(generatedText);
+
+    // JSONファイルかどうかの確認
+    function isJSON(str) {
+      try {
+        JSON.parse(str);
+        return true;
+      } catch (error) {
+        return false;
+      }
+    }
+
+    // APIリクエストを開始する前にローディング表示を開始
+    setLoading(true);
 
     try {
       // Chat.jsx にメッセージを渡して API から回答を取得
       const responseText = await Chat(generatedText);
-      console.log("レシピを思いつきました！");
-
-      // エラーが発生しなかった場合の処理
       console.log("isJSON:", isJSON(responseText));
       console.log(responseText);
-      function isJSON(str) {
-        try {
-          JSON.parse(str);
-          return true;
-        } catch (error) {
-          return false;
-        }
-      }
+
+      // エラーが発生しなかった場合の処理
       if (isJSON(responseText)) {
+        console.log("レシピを思いつきました！");
         // 回答の格納
         setAnswer(responseText);
 
@@ -129,6 +135,8 @@ export const AddAiRecipe = ({ navigation }) => {
     } catch (error) {
       // エラーが発生した場合の処理
       alert("エラーが発生しました\nもう一度実行してください: " + error.message);
+    } finally {
+      setLoading(false); // ローディング終了
     }
   };
 
@@ -164,170 +172,347 @@ export const AddAiRecipe = ({ navigation }) => {
 
   useEffect(() => {}, []);
   return (
-    <View>
-      <Controller
-        control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            style={{ borderWidth: 1, padding: 10, marginBottom: 10 }}
-            value={value}
-            onBlur={onBlur}
-            onChangeText={(value) => {
-              onChange(value);
-              handleMessageChange(value);
-            }}
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <View style={styles.inputArea}>
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={{
+                  flex: 1,
+                  backgroundColor: "white",
+                  borderColor: "gray",
+                  borderWidth: 1,
+                  padding: 8,
+                  marginBottom: 10,
+                  borderRadius: 10,
+                  fontSize: 16,
+                  color: "#444",
+                }}
+                placeholder="食材を入力してください"
+                value={value}
+                onBlur={onBlur}
+                onChangeText={(value) => {
+                  onChange(value);
+                  handleMessageChange(value);
+                }}
+              />
+            )}
+            name="userItem"
           />
-        )}
-        name="userItem"
-      />
-      <Text>を使って</Text>
-      <SelectDropdown
-        data={selectTargetData}
-        onSelect={(selectedItem, index) => {
-          handleTargetChange(selectedItem);
-        }}
-        defaultButtonText={selectedTarget}
-        buttonTextAfterSelection={(selectedItem, index) => {
-          // 選択した後に何の情報を渡すか
-          return selectedItem;
-        }}
-        rowTextForSelection={(item, index) => {
-          // プルダウンに何の情報を表示するか
-          return item;
-        }}
-      />
-      <SelectDropdown
-        data={selectFeelingData}
-        onSelect={(selectedItem, index) => {
-          handleFeelingChange(selectedItem);
-        }}
-        defaultButtonText={selectedFeeling}
-        buttonTextAfterSelection={(selectedItem, index) => {
-          // 選択した後に何の情報を渡すか
-          return selectedItem;
-        }}
-        rowTextForSelection={(item, index) => {
-          // プルダウンに何の情報を表示するか
-          return item;
-        }}
-      />
-      <SelectDropdown
-        data={selectGenreData}
-        onSelect={(selectedItem, index) => {
-          handleGenreChange(selectedItem);
-        }}
-        defaultButtonText={selectedGenre}
-        buttonTextAfterSelection={(selectedItem, index) => {
-          // 選択した後に何の情報を渡すか
-          return selectedItem;
-        }}
-        rowTextForSelection={(item, index) => {
-          // プルダウンに何の情報を表示するか
-          return item;
-        }}
-      />
-      <SelectDropdown
-        data={selectCategoryData}
-        onSelect={(selectedItem, index) => {
-          handleCategoryChange(selectedItem);
-        }}
-        defaultButtonText={selectedCategory}
-        buttonTextAfterSelection={(selectedItem, index) => {
-          // 選択した後に何の情報を渡すか
-          return selectedItem;
-        }}
-        rowTextForSelection={(item, index) => {
-          // プルダウンに何の情報を表示するか
-          return item;
-        }}
-      />
-      <SelectDropdown
-        data={selectServingData}
-        onSelect={(selectedItem, index) => {
-          handleServingChange(selectedItem);
-        }}
-        defaultButtonText={selectedServing}
-        buttonTextAfterSelection={(selectedItem, index) => {
-          // 選択した後に何の情報を渡すか
-          return selectedItem;
-        }}
-        rowTextForSelection={(item, index) => {
-          // プルダウンに何の情報を表示するか
-          return item;
-        }}
-      />
-      <Button title="考えてもらう" onPress={handleSubmit} />
-      <View>
-        <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-          考えるレシピ内容:
-        </Text>
-        <Text>{questionText}</Text>
+          <Text style={styles.text}>を使って</Text>
+        </View>
+        <View style={styles.dropDownArea}>
+          <SelectDropdown
+            data={selectTargetData}
+            onSelect={(selectedItem, index) => {
+              handleTargetChange(selectedItem);
+            }}
+            defaultButtonText={selectedTarget}
+            buttonTextAfterSelection={(selectedItem, index) => {
+              // 選択した後に何の情報を渡すか
+              return selectedItem;
+            }}
+            rowTextForSelection={(item, index) => {
+              // プルダウンに何の情報を表示するか
+              return item;
+            }}
+            buttonStyle={styles.dropdown1BtnStyle}
+            buttonTextStyle={styles.dropdown1BtnTxtStyle}
+            renderDropdownIcon={(isOpened) => {
+              return (
+                <FontAwesome
+                  name={isOpened ? "chevron-up" : "chevron-down"}
+                  color={"#444"}
+                  size={18}
+                />
+              );
+            }}
+            dropdownIconPosition={"right"}
+            dropdownStyle={styles.dropdown1DropdownStyle}
+            rowStyle={styles.dropdown1RowStyle}
+            rowTextStyle={styles.dropdown1RowTxtStyle}
+          />
+          <Text style={styles.text}>が</Text>
+          <SelectDropdown
+            data={selectFeelingData}
+            onSelect={(selectedItem, index) => {
+              handleFeelingChange(selectedItem);
+            }}
+            defaultButtonText={selectedFeeling}
+            buttonTextAfterSelection={(selectedItem, index) => {
+              // 選択した後に何の情報を渡すか
+              return selectedItem;
+            }}
+            rowTextForSelection={(item, index) => {
+              // プルダウンに何の情報を表示するか
+              return item;
+            }}
+            buttonStyle={styles.dropdown1BtnStyle}
+            buttonTextStyle={styles.dropdown1BtnTxtStyle}
+            renderDropdownIcon={(isOpened) => {
+              return (
+                <FontAwesome
+                  name={isOpened ? "chevron-up" : "chevron-down"}
+                  color={"#444"}
+                  size={18}
+                />
+              );
+            }}
+            dropdownIconPosition={"right"}
+            dropdownStyle={styles.dropdown1DropdownStyle}
+            rowStyle={styles.dropdown1RowStyle}
+            rowTextStyle={styles.dropdown1RowTxtStyle}
+          />
+        </View>
+        <View style={styles.dropDownArea}>
+          <SelectDropdown
+            data={selectGenreData}
+            onSelect={(selectedItem, index) => {
+              handleGenreChange(selectedItem);
+            }}
+            defaultButtonText={selectedGenre}
+            buttonTextAfterSelection={(selectedItem, index) => {
+              // 選択した後に何の情報を渡すか
+              return selectedItem;
+            }}
+            rowTextForSelection={(item, index) => {
+              // プルダウンに何の情報を表示するか
+              return item;
+            }}
+            buttonStyle={styles.dropdown1BtnStyle}
+            buttonTextStyle={styles.dropdown1BtnTxtStyle}
+            renderDropdownIcon={(isOpened) => {
+              return (
+                <FontAwesome
+                  name={isOpened ? "chevron-up" : "chevron-down"}
+                  color={"#444"}
+                  size={18}
+                />
+              );
+            }}
+            dropdownIconPosition={"right"}
+            dropdownStyle={styles.dropdown1DropdownStyle}
+            rowStyle={styles.dropdown1RowStyle}
+            rowTextStyle={styles.dropdown1RowTxtStyle}
+          />
+          <Text style={styles.text}>　</Text>
+          <SelectDropdown
+            data={selectCategoryData}
+            onSelect={(selectedItem, index) => {
+              handleCategoryChange(selectedItem);
+            }}
+            defaultButtonText={selectedCategory}
+            buttonTextAfterSelection={(selectedItem, index) => {
+              // 選択した後に何の情報を渡すか
+              return selectedItem;
+            }}
+            rowTextForSelection={(item, index) => {
+              // プルダウンに何の情報を表示するか
+              return item;
+            }}
+            buttonStyle={styles.dropdown1BtnStyle}
+            buttonTextStyle={styles.dropdown1BtnTxtStyle}
+            renderDropdownIcon={(isOpened) => {
+              return (
+                <FontAwesome
+                  name={isOpened ? "chevron-up" : "chevron-down"}
+                  color={"#444"}
+                  size={18}
+                />
+              );
+            }}
+            dropdownIconPosition={"right"}
+            dropdownStyle={styles.dropdown1DropdownStyle}
+            rowStyle={styles.dropdown1RowStyle}
+            rowTextStyle={styles.dropdown1RowTxtStyle}
+          />
+        </View>
+        <View style={styles.dropDownArea}>
+          <SelectDropdown
+            data={selectServingData}
+            onSelect={(selectedItem, index) => {
+              handleServingChange(selectedItem);
+            }}
+            defaultButtonText={selectedServing}
+            buttonTextAfterSelection={(selectedItem, index) => {
+              // 選択した後に何の情報を渡すか
+              return selectedItem;
+            }}
+            rowTextForSelection={(item, index) => {
+              // プルダウンに何の情報を表示するか
+              return item;
+            }}
+            buttonStyle={styles.dropdown1BtnStyle}
+            buttonTextStyle={styles.dropdown1BtnTxtStyle}
+            renderDropdownIcon={(isOpened) => {
+              return (
+                <FontAwesome
+                  name={isOpened ? "chevron-up" : "chevron-down"}
+                  color={"#444"}
+                  size={18}
+                />
+              );
+            }}
+            dropdownIconPosition={"right"}
+            dropdownStyle={styles.dropdown1DropdownStyle}
+            rowStyle={styles.dropdown1RowStyle}
+            rowTextStyle={styles.dropdown1RowTxtStyle}
+          />
+          <Text style={styles.text}>　人前のレシピ</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+            <Text style={styles.text}>AIに考えてもらう</Text>
+          </TouchableOpacity>
+        </View>
       </View>
+      {loading && (
+        <View>
+          <Text>{questionText}を考えています...</Text>
+        </View>
+      )}
       {answer && (
-        <ScrollView>
-          <View>
-            <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-              〜 AI オリジナル レシピ 〜
-            </Text>
-            <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-              レシピ名：{jsAnswer.recipeName}
-            </Text>
-            <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-              材料：{jsAnswer.serving}人前
-            </Text>
+        <View>
+          <View style={styles.titleArea}>
+            <Text style={styles.titleText}>〜 AI's オリジナル レシピ 〜</Text>
+          </View>
+          <ScrollView>
             <View>
-              {jsAnswer.recipeItems && jsAnswer.recipeItems.length > 0 ? (
-                jsAnswer.recipeItems.map((item, index) => (
-                  <Text key={index}>
-                    {item.recipeItemName}：{item.quantity}
-                    {item.unit}
-                  </Text>
-                ))
-              ) : (
-                <Text>No items found.</Text>
-              )}
+              <Text style={styles.recipeTitle}>{jsAnswer.recipeName}</Text>
+              <Text style={styles.recipeLabel}>
+                材料　＜{jsAnswer.serving}人前＞
+              </Text>
+              <View style={styles.recipeText}>
+                {jsAnswer.recipeItems && jsAnswer.recipeItems.length > 0 ? (
+                  jsAnswer.recipeItems.map((item, index) => (
+                    <Text key={index}>
+                      {item.recipeItemName}：{item.quantity}
+                      {item.unit}
+                    </Text>
+                  ))
+                ) : (
+                  <Text>No items found.</Text>
+                )}
+              </View>
+              <Text style={styles.recipeLabel}>作り方</Text>
+              <View style={styles.recipeText}>
+                {jsAnswer.memo &&
+                  jsAnswer.memo
+                    .split("\n")
+                    .map((line, index) => <Text key={index}>{line}</Text>)}
+              </View>
             </View>
-            <Text style={{ fontSize: 18, fontWeight: "bold" }}>作り方：</Text>
-            <Text>
-              {jsAnswer.memo &&
-                jsAnswer.memo.split("\n").map((line, index) => (
-                  <Text key={index}>
-                    {line}
-                    {"\n"}
-                  </Text>
-                ))}
-            </Text>
-          </View>
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => handleRecipeSubmit()}
-            >
-              <Text style={styles.button}>レシピを登録</Text>
-            </TouchableOpacity>
-          </View>
-          <Text>改行</Text>
-          <Text>改行</Text>
-          <Text>改行</Text>
-          <Text>改行</Text>
-          <Text>改行</Text>
-          <Text>改行</Text>
-          <Text>改行</Text>
-          <Text>改行</Text>
-          <Text>改行</Text>
-          <Text>改行</Text>
-          <Text>{tmpCheck}</Text>
-        </ScrollView>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleRecipeSubmit()}
+              >
+                <Text style={styles.text}>レシピを登録</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.blank}>
+              <Text></Text>
+            </View>
+          </ScrollView>
+        </View>
       )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff0d4", //買い物リストの背景色
+    padding: 10,
+  },
+  inputContainer: {
+    // flex: 1,
+  },
+  text: {
+    marginLeft: 8,
+    marginRight: 8,
+    fontSize: 16,
+  },
+  inputArea: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  dropDownArea: {
+    flexDirection: "row",
+    // justifyContent: "center",
+    alignItems: "center",
+  },
+  dropdown1BtnStyle: {
+    width: "46%",
+    height: 35,
+    backgroundColor: "#FFF",
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "gray",
+    marginBottom: 8,
+  },
+  dropdown1BtnTxtStyle: {
+    color: "#444",
+    textAlign: "left",
+    fontSize: 16,
+  },
+  dropdown1DropdownStyle: { backgroundColor: "#EFEFEF" },
+  dropdown1RowStyle: {
+    backgroundColor: "#EFEFEF",
+    borderBottomColor: "#C5C5C5",
+  },
+  dropdown1RowTxtStyle: {
+    marginLeft: 18,
+    color: "#444",
+    textAlign: "left",
+    fontSize: 16,
+  },
   buttonContainer: {
-    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
   },
   button: {
-    backgroundColor: "white",
+    margin: 8,
+    backgroundColor: "#b6c471",
+    width: 200,
+    height: 32,
+    borderRadius: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleArea: {
+    padding: 8,
+    backgroundColor: "#B45817",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleText: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold",
+  },
+  recipeTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    margin: 8,
+    marginBottom: 4,
+  },
+  recipeLabel: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginTop: 8,
+    marginLeft: 8,
+    marginBottom: 0,
+  },
+  recipeText: {
+    marginTop: 4,
+    marginLeft: 24,
+  },
+  blank: {
+    height: 320,
   },
 });
